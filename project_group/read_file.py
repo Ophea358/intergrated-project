39c8500d67b960baa7ce564a746766a28996d077
Ophea358 <109256780+Ophea358@users.noreply.github.com>
2022-07-27 13:43:02 +0800

    Update read_file.py

--- project_group/read_file.py
+++ project_group/read_file.py
@@ -1,1 +1,5 @@
-read and extract data from files
+# read and extract data from files
+# Profit & Loss :  Flag  the days and deficit amount of the net profit when the current day is lower than the previous day.
+# Cash On Hand :  Flag the days and deficit amount when the current day is lower than the previous day.
+# Overheads : Flag the  category with the highest overheads. 
+# Convert the amount flagged from part a, b and c with the mean of the weekly closing forex price from the API call.
