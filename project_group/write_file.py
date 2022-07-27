09f395636e8c65514bd31862179c7bd781a724d7
mxiyi <109279926+mxiyi@users.noreply.github.com>
2022-07-27 18:01:09 +0800

    Update write_file.py

--- project_group/write_file.py
+++ project_group/write_file.py
@@ -3,8 +3,10 @@
 from pathlib import Path
 import re,csv
 
-file_path = Path.cwd()/"project_group"
+file_path = Path.cwd()/"project_group"/"summary_report.txt"
+file_path.touch()
 print(file_path)
+print(file_path.exists())
 
 with file_path.open(mode = "w", encoding = "UTF-8", newline = "") as file:
   writer = csv.writer(file)
