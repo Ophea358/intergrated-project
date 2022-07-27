24d12ebe7dbc8146ebdd33edd4760fac59509f6d
mxiyi <109279926+mxiyi@users.noreply.github.com>
2022-07-27 17:42:06 +0800

    Update write_file.py

--- project_group/write_file.py
+++ project_group/write_file.py
@@ -1,8 +1,12 @@
 # write data into txt file
 
 from pathlib import Path
+import re,csv
 
 file_path = Path.cwd()/"project_group"
 print(file_path)
 
-with file_path.open(mode = "w", encoding = "UTF-8", newline = "
+with file_path.open(mode = "w", encoding = "UTF-8", newline = "") as file:
+  writer = csv.writer(file)
+  writer.writerow([])
+  writer.writerows( )
