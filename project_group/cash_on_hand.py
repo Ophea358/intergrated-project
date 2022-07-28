80ce19a8c7becaeaeda3d27bd05bbbf0526f7cfa
Ophea358 <109256780+Ophea358@users.noreply.github.com>
2022-07-28 17:19:11 +0800

    Update cash_on_hand.py

--- project_group/cash_on_hand.py
+++ project_group/cash_on_hand.py
@@ -3,9 +3,7 @@
 # flag = mark 
 # deficit = when money is too small / an excess of expenditure or liabilities over income or assets in a given period
 
-# Profit & Loss :  Flag  the days and deficit amount of the net profit when the current day is lower than the previous day.
 # Cash On Hand :  Flag the days and deficit amount when the current day is lower than the previous day.
-# Overheads : Flag the  category with the highest overheads. 
 # Convert the amount flagged from part a, b and c with the mean of the weekly closing forex price from the API call.
 
 # import Path function from pathlib
@@ -25,12 +23,23 @@ for file in file_path.glob("*.csv"):
 
 # csv files in the csv_report folder
 cash_on_hand = file_path/"cvs"/"Cash on Hand.csv"
-overheads = file_path/"cvs"/"Overheads.csv"
-profit_loss = file_path/"cvs"/"Profit and Loss.csv"
 
 def data_collection(data):
   with data.open(mode = 'r', encoding = 'UTF-8') as files:
           info = files.read()
           cash_on_hand = re.findall(pattern = r"", string = info)
-          overheads = re.findall(pattern = r"", string = info)
-          profit_loss = re.findall(pattern = r"", string = info)
+
+          # write data into txt file
+
+from pathlib import Path
+import re,csv
+
+file_path = Path.cwd()/"project_group"/"summary_report.txt"
+file_path.touch()
+print(file_path)
+print(file_path.exists())
+
+with file_path.open(mode = "w", encoding = "UTF-8", newline = "") as file:
+  writer = csv.writer(file)
+  writer.writerow([])
+  writer.writerows( )
