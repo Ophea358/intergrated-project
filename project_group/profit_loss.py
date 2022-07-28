5c98c8a9506e675784bb5443141077a7c18dc55c
MaltseFalcon <s10242859@connect.np.edu.sg>
2022-07-28 17:50:32 +0800

    commit

--- project_group/profit_loss.py
+++ project_group/profit_loss.py
@@ -14,34 +14,42 @@ from pathlib import Path
 import re, csv
 
 # instantiate a file path object to current working directory
-#file_path = Path.cwd()/"project_group"
+file_path = Path.cwd()/"project_group"
 #for file in file_path.glob('.csv'):
   #with open(file,mode='r',encoding='UTF-8') as files:
     #info=files.read()
 
 # iterate over the 3 csv files in the cvs_report folder
-for file in file_path.glob("*.csv"):
-    print(file)
+#for file in file_path.glob("*.csv"):
+    #print(file)
 
 # csv files in the csv_report folder
-profit_loss = file_path/"cvs"/"Profit and Loss.csv"
+profit_loss = Path.cwd()/"project_group"/"csv_reports"
+
+for file in profit_loss.glob("Profit and Loss.csv"):
+  with profit_loss.open(mode = 'r', encoding = 'UTF-8') as file:
+    target = file.read()
+  day = re.findall(pattern = "[0-9][0-9]", string = target)[0]
+  sales = re.findall(pattern = r"[0-8].+", string = target)[0]
+  trading_profit = re.findall(pattern = r"[0-8].+", string = target)[1]
+  print(trading_profit)
 
 def data_collection(data):
   with data.open(mode = 'r', encoding = 'UTF-8') as files:
-          info = files.read()
-          profit_loss = re.findall(pattern = r"", string = info)
+    info = files.read()
+  profit_loss = re.findall(pattern = r"", string = info)
 
 # write data into txt file
 
 from pathlib import Path
 import re,csv
 
-file_path = Path.cwd()/"project_group"/"summary_report.txt"
-file_path.touch()
-print(file_path)
-print(file_path.exists())
+#ile_path = Path.cwd()/"project_group"/"summary_report.txt"
+#file_path.touch()
+#print(file_path)
+#print(file_path.exists())
 
-with file_path.open(mode = "w", encoding = "UTF-8", newline = "") as file:
-  writer = csv.writer(file)
-  writer.writerow([])
-  writer.writerows( )
+#with file_path.open(mode = "w", encoding = "UTF-8", newline = "") as file:
+  #writer = csv.writer(file)
+  #writer.writerow([])
+  #writer.writerows( )
