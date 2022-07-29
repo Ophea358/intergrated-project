c899d632562632900ce738b08877e90308dd0594
Ophea358 <109256780+Ophea358@users.noreply.github.com>
2022-07-29 10:48:38 +0800

    Update api.py

--- project_group/api.py
+++ project_group/api.py
@@ -1,7 +1,23 @@
-# import forex exchange data
-import requests
+from pathlib import Path
+import requests, json, os, re, csv
 
-url = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey=GJVFL0NV2F7LZ8UV"
-response = requests.get(url)
-data = response.json()
-print(data)
+def api():
+    '''
+    retrieve real time exchange rate from the web
+    '''
+    # import forex exchange data  
+    url = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey=GJVFL0NV2F7LZ8UV"
+    response = requests.get(url)
+    data = response.text
+    print(data)
+    # convert data to json to retrieve exchange rate
+    data_info = json.loads(data)
+    datakey  = data_info["Realtime Currency Exchange Rate"]
+    exrate = datakey.get('5. Exchange Rate')
+    
+    # input real time currency conversion rate into summary report
+    file_path = Path.cwd()/"project_group"/"summary_report.txt"
+    with file_path.open(mode = "w") as file:
+        file.write(f"[REAL TIME CURRENCY CONVERSION RATE] USD1 = SGD{exrate}")
+
+print(api())
