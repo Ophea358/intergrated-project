928d9d7319581753d334ea2943d386b2ae9d6a70
Ophea358 <109256780+Ophea358@users.noreply.github.com>
2022-07-31 20:42:37 +0800

    Update main.py

--- project_group/main.py
+++ project_group/main.py
@@ -1,1 +1,17 @@
-# combination of other modules
+import api, cash_on_hand, profit_loss, overheads
+
+def main():
+    '''
+    combination of all functions
+    '''
+    # api functions
+    api.apiwrite()
+    # overheads functions
+    overheads.high_heads()
+    # cash on hand functions
+    cash_on_hand.cash_def()
+    cash_on_hand.cash_sur()
+    # profit and loss functions
+    profit_loss.net_def()
+    profit_loss.net_sur()
+print(main())
